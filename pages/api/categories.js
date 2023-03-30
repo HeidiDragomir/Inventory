93bfc3bffdeafeed41606aa06d2270aafe220384
HeidiDragomir <marinela.dragomir@chasacademy.se>
2023-03-30 14:37:15 +0200

    Testing to group products in category and subcategory

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -2,23 +2,56 @@ import fs from "fs";
 import { getProducts } from "../../scripts/products";
 
 export default async function handler(req, res) {
-	let categories = [];
 	let products = await getProducts();
+	// let productCategory
 	let groupedProducts = {};
 
 	// Group the products by category
 	for (let product of products) {
 		const productCategory = product.productNumber.toString().slice(0, 2);
+		const productCategory2 = product.productNumber.toString().slice(0, 3);
+
 		if (!groupedProducts[productCategory]) {
-			groupedProducts[productCategory] = [];
+			groupedProducts[productCategory] = {};
 		}
-		groupedProducts[productCategory].push(product);
+
+		if (!groupedProducts[productCategory][productCategory2]) {
+			groupedProducts[productCategory][productCategory2] = {};
+		}
+
+		const productNumber = product.productNumber.toString();
+		if (!groupedProducts[productCategory][productCategory2][productNumber]) {
+			groupedProducts[productCategory][productCategory2][productNumber] = [];
+		}
+
+		groupedProducts[productCategory][productCategory2][productNumber].push(
+			product
+		);
 	}
+
+	console.log(groupedProducts);
 
 	// Write each category to its own file
 	for (let category in groupedProducts) {
 		const filename = `./inventory/BK${category}.json`;
-		const categoryProducts = groupedProducts[category];
+		const categoryProducts = [];
+
+		for (let subCategory in groupedProducts[category]) {
+			const subCategoryProducts = [];
+			const productsByNumber = groupedProducts[category][subCategory];
+
+			for (let subsubCategory in productsByNumber) {
+				subCategoryProducts.push({
+					subsubCategory,
+					products: productsByNumber[subsubCategory],
+				});
+			}
+
+			categoryProducts.push({
+				subCategory,
+				products: subCategoryProducts,
+			});
+		}
 
 		fs.writeFile(filename, JSON.stringify(categoryProducts), (err) => {
 			if (err) {
--- scripts/products.js
+++ scripts/products.js
@@ -2,12 +2,11 @@ import fs from "fs/promises";
 
 export async function getProducts() {
 	let products = [];
-	let rowItems;
 
 	try {
 		const data = await fs.readFile("./inventory/Basta-inventory.csv", "utf8");
 		for (let row of data.split("\n")) {
-			rowItems = row.split(";");
+			const rowItems = row.split(";");
 			if (rowItems[4].length < 5) {
 				products.push({
 					productNumber: 0 + rowItems[4],
